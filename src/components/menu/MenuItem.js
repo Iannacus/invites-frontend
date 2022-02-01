7b2114309cdf8e126ebd1fca64b49907e600f47d
iannacus <ian.rosasmal@gmail.com>
2022-01-31 23:56:14 -0600

    change admin style

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const MenuItem = ({ icon, itemTitle }) => {
+const MenuItem = ({ icon, itemTitle, isTitle }) => {
+  const clases = isTitle ? 'logo' : 'title'
   return (
     <a href="#">
       <span className="icon">{icon}</span>
-      <span className="title">{itemTitle}</span>
+      <span className={clases} >{itemTitle}</span>
     </a>
   )
 }
